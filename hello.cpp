1c7f47792349ed7b7d356afbc3a6f5a84c496786
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-02-07 20:08:00 +0300

    feat: solution of the `first task`

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,11 @@
 #include <string>
 
 int main() {
-    std::string name;
-    std::cout << "What's your  name?\n";
-    std::cin >> name;
-    std::cout << "Hello, " << name << "!\n";
+    std::string dash = "  - ";
+    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
+    std::cout << dash << "is a better C\n";
+    std::cout << dash << "supports data abstraction\n";
+    std::cout << dash << "supports object-oriented programming\n";
+    std::cout << dash << "supports generic programming.\n";
     return 0;
 }
