d2bb5b5bc9e0a589f018397727006b4e337689e5
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-02-07 20:19:56 +0300

    feat: solution of the `second task`

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
-#include <string>
 
 int main() {
-    std::string dash = "  - ";
-    std::cout << "C++ is a general-purpose programming language with a bias towards systems programming that\n";
-    std::cout << dash << "is a better C\n";
-    std::cout << dash << "supports data abstraction\n";
-    std::cout << dash << "supports object-oriented programming\n";
-    std::cout << dash << "supports generic programming.\n";
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a+b;
     return 0;
 }
