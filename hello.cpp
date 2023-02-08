e781f477c610e37d29dc6e1d0b626e5154782484
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-02-08 20:17:20 +0300

    feat: solution of the `second task`

--- hello.cpp
+++ hello.cpp
@@ -2,14 +2,12 @@
 #include <cmath>
 
 int main() {
-    int a, b, c, d;
-    std::cin >> a >> b >> c >> d;
-    if (
-        std::abs(a - c) == std::abs(b - d) || 
-        a == c || 
-        b == d
-    ) {
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    if (pow(a, 2) == pow(b, 2) + pow(c, 2) || pow(b, 2) == pow(a, 2) + pow(c, 2) || pow(c, 2) == pow(a, 2) + pow(b, 2)) {
         std::cout << "YES";
+    } else if (a + b <= c || a + c <= b || b + c <= a) {
+        std::cout << "UNDEFINED";
     } else {
         std::cout << "NO";
     }
